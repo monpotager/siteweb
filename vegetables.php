e5daa84a595fc0a0477b0f2c2faf2a3ca7073125
Cyril Kato <cyril@flui.city>
2017-01-30 11:35:37 +0100

    fix wrong link

--- vegetables.php
+++ vegetables.php
@@ -28,12 +28,12 @@ require_once '_include/authenticate-user.php';
     <nav class="light-blue lighten-1" role="navigation">
       <div class="nav-wrapper container"><a id="logo-container" href="./" class="brand-logo">Mon Potager</a>
         <ul class="right hide-on-med-and-down">
-          <li class="active"><a href="editor.php?my_token=<?php echo $user['token']; ?>">Mon jardin</a></li>
+          <li class="active"><a href="vegetables.php?my_token=<?php echo $user['token']; ?>">Mon jardin</a></li>
           <li><a href="logout.php?my_token=<?php echo $user['token']; ?>">Se déconnecter</a></li>
         </ul>
 
         <ul id="nav-mobile" class="side-nav">
-          <li class="active"><a href="editor.php?my_token=<?php echo $user['token']; ?>">Mon jardin</a></li>
+          <li class="active"><a href="vegetables.php?my_token=<?php echo $user['token']; ?>">Mon jardin</a></li>
           <li><a href="logout.php?my_token=<?php echo $user['token']; ?>">Se déconnecter</a></li>
         </ul>
         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
